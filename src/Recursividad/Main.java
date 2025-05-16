c2e0c98813425fc7ce9181b5c9475d8a56f3d273
HugoR96 <asrivera36@gmail.com>
2025-05-16 09:08:32 -0400

    16-05-25

---
+++ src/Recursividad/Main.java
@@ -0,0 +1,9 @@
+package Recursividad;
+
+public class Main {
+    public static void main(String[]args){
+        //System.out.println(Ejercicios.factorial(10));
+
+        System.out.println(Ejercicios.mcd(412,184));
+    }
+}
